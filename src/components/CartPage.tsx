ef9a009e3f0e584544c0628758ef300e756fbd12
VacBal <vaczi.balazs.zsolt@gmail.com>
2024-11-29 14:04:52 +0100

    Javítások
    Próba egy

--- src/components/CartPage.tsx
+++ src/components/CartPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 const CartPage: React.FC = () => {
     const [cart, setCart] = useState<any[]>([]);
   
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const ProductList: React.FC = () => {
     const [products, setProducts] = useState<any[]>([]);
